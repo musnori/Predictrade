a235d660a9b2555140104975e689f5a7a8154882
musnori <yasunori0217@docomo.ne.jp>
2025-12-21 12:56:14 +0900

    Add history page and user history API

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
+// vite.config.js
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: { port: 5173 },
   build: {
     rollupOptions: {
       input: {
-        index: "index.html",
+        main: "index.html",
         create: "create.html",
-        event: "event.html",
         dashboard: "dashboard.html",
+        event: "event.html",
+        history: "history.html", // ← これ追加
       },
     },
   },
